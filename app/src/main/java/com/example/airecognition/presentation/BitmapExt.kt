4fcaf4ab89a23396431ba5cbe357028c93052a97
Mohammad Tarek Hendi <t.hindi@outlook.com>
2023-10-26 17:53:14 +0800

    * Create BitmapExt which achieve centerCrop.

---
+++ app/src/main/java/com/example/airecognition/presentation/BitmapExt.kt
@@ -0,0 +1,17 @@
+package com.example.airecognition.presentation
+
+import android.graphics.Bitmap
+
+fun Bitmap.centerCrop(desiredWidth: Int, desiredHeight: Int): Bitmap {
+    val xStart = (width - desiredWidth) / 2
+    val yStart = (height - desiredWidth) / 2
+
+    // If some reason is not valid
+    if (xStart < 0 || yStart < 0 || desiredWidth > width || desiredHeight > height) {
+        throw IllegalArgumentException("Invalid arguments for center cropping")
+    }
+
+    return Bitmap.createBitmap(this, xStart, yStart, desiredWidth, desiredHeight)
+}
+
+// In this class we achieve the center crop effect (321 x 321)
